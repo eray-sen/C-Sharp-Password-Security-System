81a901f4508abb86275008c396bf9cf7cd028c1d
eray-sen <eray.sen2@ogr.sakarya.edu.tr>
2022-12-12 23:11:06 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,4 @@
-﻿/****************************************************************************
-**                    
-**                          SAKARYA ÜNİVERSİTESİ
-**              BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
-**                   BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
-**                     NESNEYE DAYALI PROGRAMLAMA DERSİ
-**                          2021-2022 BAHAR DÖNEMİ
-**
-**
-**                         ÖDEV NUMARASI: 1
-**                         ÖĞRENCİ ADI: ERAY ŞEN
-**                         ÖĞRENCİ NUMARASI: G211210019
-**                         DERS GRUBU: 2B
-******************************************************************************/
-
-using System;
+﻿using System;
 
 namespace NesneyeDayaliProgramlamaOdev1
 {
